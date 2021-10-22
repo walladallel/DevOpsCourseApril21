09afa863e2b2ffd5f0be1ddf5a3078582644f4ae
Alon Itach <alon.itach@mail.huji.ac.il>
2021-10-22 15:07:04 +0300

    fix

--- expand_object_globally.py
+++ expand_object_globally.py
@@ -2,33 +2,26 @@ import json
 import logging
 import boto3
 from botocore.exceptions import ClientError
-import os
 
 logging.basicConfig(level=logging.INFO)
 
 file_name = 'secret.txt'
 
 
-def upload_file(agent_name, bucket, object_name=None):
+def upload_file(agent_name, bucket):
     """Upload a file to an S3 bucket
 
     :param agent_name:
     :param bucket: Bucket to upload to
-    :param object_name: S3 object name. If not specified then file_name is used
     """
-
-    # If S3 object_name was not specified, use file_name
-    if object_name is None:
-        object_name = os.path.basename(file_name)
 
     # Upload the file
     s3_client = boto3.client('s3')
     try:
-        response = s3_client.upload_file(file_name, bucket, object_name)
+        response = s3_client.upload_file(file_name, bucket)
         logging.info(f'Uploading file to agent {agent_name} has been finished with response: {response}')
     except ClientError as e:
         logging.error(e)
-        return False
 
 
 if __name__ == '__main__':
