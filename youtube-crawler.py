1513b19bc8eb6037fd1c2ad744f2b658813ecc45
eyal <eyal5000192@gmail.com>
2021-11-17 21:00:56 +0200

    Update youtube-crawler.py

--- youtube-crawler.py
+++ youtube-crawler.py
@@ -6,15 +6,6 @@ from botocore.exceptions import ClientError
 
 
 def search_download(search_str, search_results):
-    """
-    This function gets a search string and download the first search_results results
-    If you don't want to download the file every run, just to get the result metadata, change below
-    download=True to download=False
-
-    :param search_str:
-    :param search_results: number of results to download
-    :return: a list of downloaded filenames
-    """
     with YoutubeDL({'format': 'bestaudio', 'noplaylist': 'False'}) as ydl:
         videos = ydl.extract_info(f"ytsearch{search_results}:{search_str}", download=True)['entries']
         return [ydl.prepare_filename(video) for video in videos]
@@ -24,9 +15,4 @@ if __name__ == '__main__':
    downloaded_files = search_download('nirvana', 1)
 
     # TODO use downloaded_files and complete a few lines to upload them to an S3 bucket
-s3_client = boto3.client('s3')
-    try:
-        response = s3_client.upload_file({'format': 'bestaudio', 'noplaylist': 'False'})
-        logging.info(f'Uploading file to agent {'format': 'bestaudio', 'noplaylist': 'False'} has been finished with response: {response}')
-    except ClientError as e:
-        logging.error(e)
+
