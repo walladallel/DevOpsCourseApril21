cd61a8e7a7735a9035bba9831d3583448ef2147e
Alon Itach <alon.itach@mail.huji.ac.il>
2021-11-18 17:26:12 +0200

    insert row to dynamo

--- insert_row_dynamodb.py
+++ insert_row_dynamodb.py
@@ -3,16 +3,6 @@ import boto3
 client = boto3.client('dynamodb')
 
 client.put_item(
-    TableName='students',
-    Item={
-        'studentId': {
-            'S': '123'
-        },
-        'name': {
-            'S': 'alon'
-        },
-        'lastname': {
-            'S': 'itach'
-        }
-    }
+    TableName='',
+    Item={}
 )
