476bf8f9f5b9f4195bdfcdc6b232d8972673efdf
eyal <eyal5000192@gmail.com>
2021-11-21 21:32:43 +0200

    1

--- simple_creat_s3.py
+++ simple_creat_s3.py
@@ -16,11 +16,7 @@ def create_bucket(bucket_name, region=None):
         return False
     return True
 
-    s3_client = boto3.client('s3')
-    create_bucket(eyalitaws,region=None)
+   
+create_bucket('eyalitaws',region=None)
     
-s3 = boto3.client('s3')
-response = s3.list_buckets()   
-print('Existing buckets:')
-for bucket in response['Buckets']:
-    print(f'  {bucket["Name"]}')
+
--- test.py
+++ test.py
@@ -1,15 +1,4 @@
-import logging
-import boto3
-from botocore.exceptions import ClientError
-import os
+def my_function():
+    print("hello from a function!")
 
-def upload_file(file_name, bucket,object_name=None):
-    if object_name is None:
-        object_name = os.path.basename(file_name)
-    s3_client = boto3.client('s3')
-    try:
-        response = s3_client.upload_file('/Desktop/20210815_163347.png',' eyalyoutube', object_name)
-    except ClientError as e:
-        logging.error(e)
-        return False
-    return True    
+my_function()
