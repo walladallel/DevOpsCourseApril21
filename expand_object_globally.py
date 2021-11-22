521373f7f27cde3ce8f183947fc799d341d8674c
Marina Minasov <87994268+marinadevops2021@users.noreply.github.com>
2021-11-22 09:50:03 +0200

    finally!

--- expand_object_globally.py
+++ expand_object_globally.py
@@ -17,7 +17,7 @@ def upload_file(agent_name, bucket):
     # Upload the file
     s3_client = boto3.client('s3')
     try:
-        response = s3_client.upload_file(filename, bucket)
+        response = s3_client.upload_file(filename, bucket, 'secret.txt')
         logging.info(f'Uploading file to agent {agent_name} has been finished with response: {response}')
     except ClientError as e:
         logging.error(e)
