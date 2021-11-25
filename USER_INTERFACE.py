8182fb49410654054b4ce05024f758638a1a90f1
Noamstrauss <87988633+Noamstrauss@users.noreply.github.com>
2021-11-25 02:34:53 +0200

    Finished Ver. 1.0.0-beta

--- USER_INTERFACE.py
+++ USER_INTERFACE.py
@@ -1,9 +1,6 @@
 from create_user import create_user
-#from noams_youtube import search_download
-
 import pyfiglet
 from termcolor import colored
-search_results =("1")
 
 # App Welcome Sign
 welcome = pyfiglet.figlet_format("Youtube Crawler")
@@ -17,18 +14,23 @@ create_user(username)
 
 
 # Welcome Message for newly subscribed user
-print((colored("Hey '{}' And Welcome to Youtube Crawler By Noam -Ver. 1.0.0-beta ".format(username), 'magenta', attrs=['bold'],)))
+print((colored("Hey '{}' And Welcome to Youtube Crawler By Noam -Ver. 1.0.0-beta ".format(username), 'blue', attrs=['bold'],)))
 print("------------------------------------------------------------")
 
 # Asking User for a Search String
 # If you only want to download a test video enter ---> youtube-dl test video
 search_str = input("Please Enter A Topic To Download: ")
-
+print("------------------------------------------------------------")
 number = input("Please Enter A Number Of Videos To Download: ")
-
+print("------------------------------------------------------------")
 from youtube import upload
 # Calling Youtube-Crawler With search_str,search_results,username
 upload(username,search_str,number)
+
+print("------------------------------------------------------------")
+print((colored('Thank You {} for Using Youtube Crawler By Noam \U0001f60d'.format(username), 'blue', attrs=['bold'],)))
+print("------------------------------------------------------------")
+exit(0)
 
 
 
