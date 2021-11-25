c146a828bd2d9b99dd0a608aafbdd109bf5717b7
Alon Itach <alon.itach@mail.huji.ac.il>
2021-11-25 16:54:18 +0200

    empty terraform file

---
+++ Jenkinsfile
@@ -0,0 +1,11 @@
+pipeline {
+  agent any
+  stages {
+    stage('Test') {
+      steps {
+        sh 'python -m unittest'
+      }
+    }
+
+  }
+}
--- test.py
+++
@@ -1,9 +0,0 @@
-def create2_user(client, *args):
-    print()
-
-
-create1_user('myclient', 1, None, 'ffff')
-
-
-create1_user('myclient', 1, None, 'ffff')
-
