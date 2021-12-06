8e123c978e410ce84f87bcf106db45cf9c27790f
Alon Itach <alon.itach@mail.huji.ac.il>
2021-12-06 19:22:06 +0200

    upgrade aws terraform provider

--- infra/main.tf
+++ infra/main.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.28"
+      version = "~> 3.68"
     }
   }
 
-  required_version = ">= 0.14.9"
+    required_version = ">= 0.14.10"
 }
 
 provider "aws" {
